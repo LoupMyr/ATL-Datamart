bbed254b1f5534662f5379f17f8acedd319fb47d
Loup Mayeur <loupmayeur2003@gmail.com>
2025-02-13 16:15:17 +0100

    tp1

--- src/data/grab_parquet.py
+++ src/data/grab_parquet.py
@@ -1,26 +1,40 @@
 from minio import Minio
 import urllib.request
-import pandas as pd
 import sys
+import os
 
 def main():
     grab_data()
     
 
 def grab_data() -> None:
-    """Grab the data from New York Yellow Taxi
+    base_url = "https://d37ci6vzurychx.cloudfront.net/trip-data/"
+    destination_folder = os.path.abspath("data/raw")
 
-    This method download x files of the New York Yellow Taxi. 
+    os.makedirs(destination_folder, exist_ok=True)
+
+    file_type = "yellow_tripdata_2024-"
     
-    Files need to be saved into "../../data/raw" folder
-    This methods takes no arguments and returns nothing.
-    """
+    for month in range(1, 9):
+        month_str = f"{month:02d}"  
+        
+        url = f"{base_url}{file_type}{month_str}.parquet"
+        file_path = os.path.join(destination_folder, f"{file_type}{month_str}.parquet")
+        
+        try:
+            # Vérifier si le fichier existe sur le serveur avant de télécharger
+            response = urllib.request.urlopen(url)
+            if response.status == 200:
+                print(f"Téléchargement de {url}")
+                urllib.request.urlretrieve(url, file_path)
+                print(f"Fichier téléchargé")
+        except Exception as e:
+            print(f"Erreur : {e}")
 
 
 def write_data_minio():
     """
-    This method put all Parquet files into Minio
-    Ne pas faire cette méthode pour le moment
+    Cette méthode transfère tous les fichiers Parquet de 'BIGDATA/data/raw' vers Minio.
     """
     client = Minio(
         "localhost:9000",
@@ -28,12 +42,25 @@ def write_data_minio():
         access_key="minio",
         secret_key="minio123"
     )
-    bucket: str = "NOM_DU_BUCKET_ICI"
+    
+    # Utilisez un nom de bucket valide
+    bucket = "tp1"
     found = client.bucket_exists(bucket)
     if not found:
         client.make_bucket(bucket)
+        print(f"Bucket {bucket} créé.")
     else:
-        print("Bucket " + bucket + " existe déjà")
+        print(f"Bucket {bucket} existe déjà.")
+
+    directory_path = "data/raw"
+    for filename in os.listdir(directory_path):
+        if filename.endswith(".parquet"):
+            file_path = os.path.join(directory_path, filename)
+            try:
+                client.fput_object(bucket, filename, file_path)
+                print(f"{filename} téléchargé dans {bucket}.")
+            except Exception as e:
+                print(f"Erreur lors du téléchargement de {filename} : {e}")
 
 if __name__ == '__main__':
     sys.exit(main())
